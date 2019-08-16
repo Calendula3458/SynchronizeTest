bd5dbcb9190f8e24248eda38adc4d78ec03c932d
Calendula3458 <275160773@qq.com>
2019-08-16 16:55:36 +0800

    wwwwww

--- SynchronizeTest/src/com/git/test/Test.java
+++ SynchronizeTest/src/com/git/test/Test.java
@@ -2,12 +2,6 @@ package com.git.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Test.main()");
-		System.out.println("aa");
-		System.out.println("Test.main()---");
-		System.out.println("bbb");
-		System.out.println("mmmmmmmmmm");
-		System.out.println("1111111");
-		System.err.println("ggggggg");
+		System.out.println("黄江");
 	}
 }
